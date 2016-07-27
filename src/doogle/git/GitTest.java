bc6555cfe4e5f4fdb1c5f45771120c70e8d4e840
lawsn <lawsnland@gmail.com>
2016-07-27 17:03:17 +0900

    IntelliJ Git Test Project. First Commit.

---
+++ src/doogle/git/GitTest.java
@@ -0,0 +1,13 @@
+package doogle.git;
+
+/**
+ * Created by doogle on 2016-07-27.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, git & github.");
+
+    }
+}
